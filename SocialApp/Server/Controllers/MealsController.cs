4b95660be23a6977681b2ea29ce5634599384a72
Andrei Ungureanu <andreiungureanu2014@yahoo.ro>
2025-05-03 20:51:31 +0300

    POST for meals DONE and left an example in the controller for testing post

--- SocialApp/Server/Controllers/MealsController.cs
+++ SocialApp/Server/Controllers/MealsController.cs
@@ -26,6 +26,29 @@ namespace Server.Controllers
             this.mealRepository.CreateMealAsync(meal);
             return this.Ok();
         }
+        /*
+         * Due to some validation for the fields, I live here an example for testing POST
+        {
+          "name": "Chicken Salad",
+          "ingredients": "Chicken, Lettuce, Tomatoes",
+          "calories": 350,
+          "category": "Salad",
+          "protein": 30,
+          "carbohydrates": 10,
+          "fat": 15,
+          "fiber": 5,
+          "sugar": 3,
+          "photoLink": "http://example.com/photo.jpg",
+          "recipe": "Mix all ingredients and serve cold.",
+          "preparationTime": 15.5,
+          "servings": 2,
+          "createdAt": "2024-05-02T10:30:00Z",
+          "cookingLevel": "Beginner",
+          "image": "/9j/4A==",
+          "imagePath": "images/chickensalad.jpg"
+        }
+        */
+
 
         [HttpGet]
         public ActionResult<List<Meal>> GetAllMeals()
