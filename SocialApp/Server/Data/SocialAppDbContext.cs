4207cd68719f112debfed5e3ef7c1643d586d553
MariaDariaTompea <mariatompea@gmail.com>
2025-05-03 17:43:49 +0300

    comment implemented

--- SocialApp/Server/Data/SocialAppDbContext.cs
+++ SocialApp/Server/Data/SocialAppDbContext.cs
@@ -12,6 +12,7 @@ namespace Server.Data
         }
 
         public DbSet<Post> Posts { get; set; } = default!;
+        public DbSet<Comment> Comments { get; set; } = default!;
         public DbSet<UserFollower> UserFollowers { get; set; } = default!;
         public DbSet<GroupUser> GroupUsers { get; set; } = default!;
 
---
+++ SocialApp/Server/Repos/CommentRepository.cs
@@ -0,0 +1,88 @@
+﻿using AppCommonClasses.Models;
+using AppCommonClasses.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialApp.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly SocialAppDbContext dbContext;
+
+        public CommentRepository(SocialAppDbContext context)
+        {
+            dbContext = context;
+        }
+
+        /// <summary>
+        /// Retrieves all comments from the database.
+        /// </summary>
+        /// <returns>A list of all Comment entities in the system.</returns>
+        public List<Comment> GetAllComments()
+        {
+            return dbContext.Comments.ToList();
+        }
+
+        /// <summary>
+        /// Retrieves all comments associated with a specific post.
+        /// </summary>
+        /// <param name="postId">The ID of the post to retrieve comments for.</param>
+        /// <returns>A list of Comment entities for the specified post.</returns>
+        public List<Comment> GetCommentsByPostId(long postId)
+        {
+            return dbContext.Comments.Where(c => c.PostId == postId).ToList();
+        }
+
+        /// <summary>
+        /// Deletes a comment from the database by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the comment to delete.</param>
+        public void DeleteCommentById(long id)
+        {
+            var comment = dbContext.Comments.Find(id);
+            if (comment != null)
+            {
+                dbContext.Comments.Remove(comment);
+                dbContext.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// Retrieves a single comment by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the comment to retrieve.</param>
+        /// <returns>The Comment entity with the specified ID, or null if not found.</returns>
+        public Comment? GetCommentById(long id)
+        {
+            return dbContext.Comments.Find(id);
+        }
+
+        /// <summary>
+        /// Saves a new comment to the database.
+        /// </summary>
+        /// <param name="entity">The Comment entity to be saved.</param>
+        public void SaveComment(Comment entity)
+        {
+            dbContext.Comments.Add(entity);
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Updates the content of an existing comment.
+        /// </summary>
+        /// <param name="id">The ID of the comment to update.</param>
+        /// <param name="content">The new content for the comment.</param>
+        public void UpdateCommentContentById(long id, string content)
+        {
+            var comment = dbContext.Comments.Find(id);
+            if (comment != null)
+            {
+                comment.Content = content;
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
