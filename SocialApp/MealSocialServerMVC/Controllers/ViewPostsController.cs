b822cdd62216a739151607c65f75190cf2476c82
Valasa-Andrei <andreivalasa7@gmail.com>
2025-05-17 18:08:14 +0300

    Added ViewPosts page to Web

---
+++ SocialApp/MealSocialServerMVC/Controllers/ViewPostsController.cs
@@ -0,0 +1,31 @@
+﻿using AppCommonClasses.Interfaces;
+using AppCommonClasses.Models;
+using MealSocialServerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+//localhost/posts
+namespace MealSocialServerMVC.Controllers
+{
+
+    [Route ("posts")]
+    public class ViewPostsController : Controller
+    {
+        private readonly IPostService postService;
+
+        public ViewPostsController(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            List<Post> posts = postService.GetAllPosts(); // You should have this in your IPostService
+            return View(posts);
+        }
+    }
+}
